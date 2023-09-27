f52b16579b2bf4711564eec3c80fc7346515ca79
therow <120190636+arxbotzz@users.noreply.github.com>
2023-09-27 13:32:50 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 clear
 pkg update && pkg upgrade -y
-pkg install git php python python2 nano figlet nmap curl wget nodejs nodejs-lts toilet openssh -y
+pkg install git php python python2 nano figlet nmap curl wget nodejs toilet openssh -y
 clear
