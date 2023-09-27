48ae46ff938db807f68b711401d926d8ed0b0eaf
arya <mukthiep@gmail.com>
2023-09-27 22:37:33 +0700

    arya

--- download.sh
+++ download.sh
@@ -1,4 +1,9 @@
 clear
+cd module
+python2 masuk.py
+clear
 pkg update && pkg upgrade -y
 pkg install git php python python2 nano figlet nmap curl wget nodejs toilet openssh -y
-clear
+cd module
+sh terahir.sh
+
