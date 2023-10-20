20962a697597061512177dde53fa3d698c5e9e34
therow <120190636+arxbotzz@users.noreply.github.com>
2023-10-20 22:02:55 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,6 +4,13 @@ python2 masuk.py
 clear
 pkg update && pkg upgrade -y
 pkg install git php python python2 nano figlet nmap curl wget nodejs toilet openssh -y
+cd
+pkg update && pkg upgrade
+pkg i -y git bc
+git clone --depth=1 https://github.com/mayTermux/myTermux.git
+cd myTermux
+export COLUMNS LINES
+./install.sh
+cd awal_awal_menggunakan_termux
 cd module
 sh terahir.sh
-
